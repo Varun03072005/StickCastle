fa8c65d735ab4228f95be59da8555141fc05a0b5
Varun03072005 <62499738+Varun03072005@users.noreply.github.com>
2020-04-11 10:09:41 +0530

    Add files via upload
    stick castle made using rectangles and triangles

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(800,400);
+  
+}
+
+function draw() {
+  background("black");
+  
+  fill("white");
+  rect(400,370,150,50);
+
+  rect(400,220,30,150);
+  
+  rect(520,220,30,150);
+
+  rect(400,200,150,30);
+
+  fill("white");
+  triangle(400,200,477,120,549,200);
+  
+}
